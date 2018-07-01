7333694beaaa0005da4769cdbb08eff6cacc792a
Akram Husain <Akram.Husain@gmail.com>
2018-07-01 13:54:58 -0700

    This is a commit to test to locl repo

--- src/com/Akram/Main.java
+++ src/com/Akram/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("This was edited from GitHub");
 	System.out.println("Only commit source changes");
+	System.out.println("To local");
+
     }
 }
